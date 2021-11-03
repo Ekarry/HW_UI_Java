1aa10a53b485c5bdb01cfd78e0f3acef1ca267a2
Ekarry <81385639+Ekarry@users.noreply.github.com>
2021-11-04 00:31:13 +0300

    Домашняя работа № 4

---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,42 @@
+
+import Triangel.TriangleApp;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TriangleTest {
+    private static Logger logger = LoggerFactory.getLogger(TriangleTest.class);
+
+    @Nested
+    @DisplayName("Тесты для треугольника:")
+    class UnitTests {
+
+        @Test
+        @DisplayName("Треугольник нельзя создать со нулевыми значениями сторон")
+        void zeroAreaTriangleCanNotBeCreated() {
+            assertThrows(IllegalArgumentException.class,() -> new TriangleApp(0,0,0));
+        }
+
+        @Test
+        @DisplayName("Треугольник нельзя создать c отрицательными значениями сторон")
+        void negativeLengthSideTriangleCanNotBeCreated() {
+            assertThrows(IllegalArgumentException.class,() -> new TriangleApp(-3,-100,2));
+        }
+
+        @Test
+        @DisplayName( ("Треугольник создается без ошибок"))
+        void validTriangleCanBeCreated() {
+            new TriangleApp(3,4,5);
+        }
+
+        @Test
+        @DisplayName("Площадь треугольника рассчитывается без ошибок")
+        void areaCalculation() {
+            TriangleApp triangle = new TriangleApp(3,4,5);
+            assertEquals(triangle.area(),6);
+            logger.info("Test is successful");
+        }
+    }
+}
